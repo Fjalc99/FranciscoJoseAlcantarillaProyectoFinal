2ccc6a9cd25c31c3e2e0d4faed1a3dc606f62906
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-05-08 10:14:16 +0200

    Proyecto

--- FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/CategoriaController.java
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/CategoriaController.java
@@ -2,13 +2,19 @@ package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.control
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.service.CategoriaService;
 
 @Controller
+@RequestMapping("/admin")
 public class CategoriaController {
 
 	@Autowired
 	private CategoriaService categoriaService;
+	
+
+	
 	
 }
