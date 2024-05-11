32245fbe18d1ea75a516f6a8806b111ecce3a0ba
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-05-11 19:35:25 +0200

    Pk_lineaDeVenta

---
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/model/LineaDeVentaPk.java
@@ -0,0 +1,20 @@
+package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LineaDeVentaPk implements Serializable {
+
+	
+	private static final long  serialVersionUID = 1L;
+	
+	private Venta venta;
+	private Long id;
+	
+}
