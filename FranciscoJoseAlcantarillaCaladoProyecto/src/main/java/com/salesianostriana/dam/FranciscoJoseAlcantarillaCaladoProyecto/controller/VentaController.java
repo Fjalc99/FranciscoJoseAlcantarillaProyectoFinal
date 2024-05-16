00483ae5c8a7f95d47026c0279957e4886c1ac15
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-05-16 19:25:21 +0200

    Vista de venta

--- FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/VentaController.java
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/VentaController.java
@@ -1,26 +1,32 @@
 package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.model.Usuario;
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.service.VentaService;
+
 
 @Controller
 public class VentaController {
 
-	@Autowired
 	private VentaService ventaService;
+	
 		
 	
 	
-	@GetMapping("/ventas")
-	public String listaVentas (Model model) {
-		model.addAttribute("venta", ventaService.findAll());
-		
-		return "misVentas";
-	}
+	@GetMapping("/Ventas")
+    public String misVentas(@AuthenticationPrincipal Usuario usuario , Model model) {
+        model.addAttribute("usuario", new Usuario());
+        model.addAttribute("ventas", usuario.getVenta());
+        return "misVentas";
+    }
+	
 	
 }
+
+
--- FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/service/VentaService.java
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/service/VentaService.java
@@ -1,12 +1,23 @@
 package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.service;
 
+
+
+
+
 import org.springframework.stereotype.Service;
 
+
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.model.Venta;
+
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.repository.VentaRepository;
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.service.base.BaseServiceImpl;
 
 @Service
 public class VentaService extends BaseServiceImpl<Venta, Long, VentaRepository> {
 
+	
+	 
+
+
+	
 }
