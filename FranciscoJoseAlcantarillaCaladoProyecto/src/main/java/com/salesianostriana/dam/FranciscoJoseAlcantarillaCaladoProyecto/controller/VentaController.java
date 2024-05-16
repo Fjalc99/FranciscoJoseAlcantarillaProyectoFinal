79de65bf75bdec8d51ec7b29d81044f6f8a71fbc
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-05-16 14:29:31 +0200

    Vista venas

--- FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/VentaController.java
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/controller/VentaController.java
@@ -1,13 +1,26 @@
 package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.service.VentaService;
 
 @Controller
 public class VentaController {
 
-
+	@Autowired
 	private VentaService ventaService;
+		
+	
+	
+	@GetMapping("/ventas")
+	public String listaVentas (Model model) {
+		model.addAttribute("venta", ventaService.findAll());
+		
+		return "misVentas";
+	}
+	
 }
