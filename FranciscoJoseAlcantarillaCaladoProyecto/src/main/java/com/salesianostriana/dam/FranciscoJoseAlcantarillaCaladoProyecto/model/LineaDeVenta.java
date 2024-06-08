bfbf86e955d2835584bf975293445cceaca63954
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-06-08 16:07:58 +0200

    Minimos arreglos

--- FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/model/LineaDeVenta.java
+++ FranciscoJoseAlcantarillaCaladoProyecto/src/main/java/com/salesianostriana/dam/FranciscoJoseAlcantarillaCaladoProyecto/model/LineaDeVenta.java
@@ -2,20 +2,19 @@ package com.salesianostriana.dam.FranciscoJoseAlcantarillaCaladoProyecto.model;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @NoArgsConstructor
